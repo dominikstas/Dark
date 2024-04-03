d96f021268cd876a296b2f35d9e075c06c72e904
Dominik Stasiak <d.stasiak436@gmail.com>
2024-04-03 20:24:14 +0200

    first commit

---
+++ Dark.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+
+procedure Dark is
+   begin
+   Put_Line("Welcome to Dark Password Generator!");
+end Dark;
